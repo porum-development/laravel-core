2ccf9eec21d1906d391da252fdd70ceba944abd8
Vinicius Medeiros <vinicius@devplace.com.br>
2019-02-16 12:34:24 -0200

    ajustes assets

--- src/resources/views/layouts/simple.blade.php
+++ src/resources/views/layouts/simple.blade.php
@@ -21,13 +21,10 @@
         @yield('css_before')
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,400i,600,700">
         <link rel="stylesheet" id="css-main" href="{{ mix('/css/codebase.css') }}">
-
-        <!-- You can include a specific file from public/css/themes/ folder to alter the default color theme of the template. eg: -->
-        <!-- <link rel="stylesheet" id="css-theme" href="{{ mix('/css/themes/corporate.css') }}"> -->
         @yield('css_after')
 
         <!-- Scripts -->
-        <script>window.Laravel = {!! json_encode(['csrfToken' => csrf_token(),]) !!};</script>
+        <script>window.Laravel = {!! json_encode(['csrfToken' => csrf_token()]) !!};</script>
     </head>
     <body>
         <!-- Page Container -->
@@ -88,8 +85,8 @@
         <!-- Codebase Core JS -->
         <script src="{{ mix('js/codebase.app.js') }}"></script>
 
-        <!-- Laravel Scaffolding JS -->
-        <script src="{{ mix('js/laravel.app.js') }}"></script>
+        <!-- Core Scaffolding JS -->
+        <script src="{{ mix('js/core.app.js') }}"></script>
 
         @yield('js_after')
     </body>
